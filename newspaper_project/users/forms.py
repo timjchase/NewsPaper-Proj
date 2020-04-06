2b269d3b910c582be36ebb988bf829d6d86dc0c2
Tim Chase <timjchase@gmail.com>
2020-04-06 14:11:25 -0400

    Chapter9 Initial

--- newspaper_project/users/forms.py
+++ newspaper_project/users/forms.py
@@ -7,11 +7,18 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
-
+        fields = (
+            "username",
+            "email",
+            "age",
+        )
